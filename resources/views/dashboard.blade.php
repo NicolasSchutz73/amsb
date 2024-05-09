c4e9d2f273220ed34d5509e024ee829d146af6be
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-09 23:08:52 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,30 +5,13 @@
         </h2>
     </x-slot>
 
-
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                {{ __("Test!") }}
-
-                </div>
-
-
-
-
-            </div>
-        </div>
-    </div>
-
-    @if(isset($instagram))
-        @foreach($instagram as $post)
-            <img src="{{ $post['url'] }}" alt="Instagram Post">
-            <p>{{ $post['caption'] ?? 'No caption' }}</p>
-        @endforeach
+    @if(isset($error))
+        <p>{{ $error }}</p>
     @else
-        <p>No Instagram posts to display or Instagram profile not configured.</p>
+        @foreach($instagram as $post)
+            <img src="{{ $post->url }}" alt="Instagram Post">
+            <p>{{ $post->caption ?? 'No caption' }}</p>
+        @endforeach
     @endif
 
 
