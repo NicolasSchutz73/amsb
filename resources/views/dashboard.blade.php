3dc4e2f2baba6d14866bcb942c32deee2732b88b
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-10 11:14:31 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,30 @@
         </h2>
     </x-slot>
 
-    <div class="grid md:grid-cols-3">
-        <div> Col 1 </div>
-        <div> Col 2 </div>
-        <div> Col 3 </div>
-    </div>
+    @if(isset($error))
+        <p class="text-red-500">{{ $error }}</p>
+    @else
+        <div class="container">
+            @foreach($instagram as $post)
+                <item>
+                    <img src="{{ $post->url }}" alt="Instagram Post" class="w-full object-cover rounded-lg">
+                </item>
+            @endforeach
+        </div>
+    @endif
+
+    <style>
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+
+        .item {
+            width: 32%;
+            padding-bottom: 32%; /* Same as width, sets height */
+            margin-bottom: 2%; /* (100-32*3)/2 */
+            position: relative;
+        }
+    </style>
 </x-app-layout>
