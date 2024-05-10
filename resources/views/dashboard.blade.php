e2aceecf496752af9eae9d086c4c3a0f0f603cc8
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-10 11:06:09 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,24 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="flex flex-wrap -mx-2">
-        <div class="p-2 w-1/3">
-            <img src="https://source.unsplash.com/random/300x300?sig=1" alt="Photo 1" class="w-full h-auto">
-        </div>
-        <div class="p-2 w-1/3">
-            <img src="https://source.unsplash.com/random/300x300?sig=2" alt="Photo 2" class="w-full h-auto">
-        </div>
-        <div class="p-2 w-1/3">
-            <img src="https://source.unsplash.com/random/300x300?sig=3" alt="Photo 3" class="w-full h-auto">
-        </div>
-        <div class="p-2 w-1/3">
-            <img src="https://source.unsplash.com/random/300x300?sig=4" alt="Photo 4" class="w-full h-auto">
-        </div>
-        <div class="p-2 w-1/3">
-            <img src="https://source.unsplash.com/random/300x300?sig=5" alt="Photo 5" class="w-full h-auto">
-        </div>
-        <div class="p-2 w-1/3">
-            <img src="https://source.unsplash.com/random/300x300?sig=6" alt="Photo 6" class="w-full h-auto">
-        </div>
+    <div class="grid md:grid-cols-3">
+        <div> Col 1 </div>
+        <div> Col 2 </div>
+        <div> Col 3 </div>
     </div>
 </x-app-layout>
