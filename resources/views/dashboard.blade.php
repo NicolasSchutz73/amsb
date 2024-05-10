eeb31e67ae1d1e87e36c310486d8433c7e010d9e
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-10 10:42:24 +0200

    fix grid

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,18 +5,17 @@
         </h2>
     </x-slot>
 
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-4">
         @if(isset($error))
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg p-4">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
                 <p class="text-red-500">{{ $error }}</p>
             </div>
         @else
-            <div class="grid grid-cols-3 gap-4">
+            <!-- Grid container -->
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 @foreach($instagram as $post)
-                    <div class="bg-white overflow-hidden shadow rounded-lg">
-                        <div class="px-4 py-5 sm:p-6">
-                            <img src="{{ $post->url }}" alt="Instagram Post" class="w-full h-auto object-cover rounded-lg">
-                        </div>
+                    <div class="bg-white rounded-lg shadow overflow-hidden">
+                        <img src="{{ $post->url }}" alt="Instagram Post" class="w-full h-auto">
                     </div>
                 @endforeach
             </div>
