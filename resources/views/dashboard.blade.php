066a3f79b62b805709bc6899c13c0884e12eff54
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-10 11:03:33 +0200

    fix grid

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,12 +5,24 @@
         </h2>
     </x-slot>
 
-    <div class="grid grid-cols-3 gap-4 p-4">
-        <div><img src="https://source.unsplash.com/random/300x300?sig=1" alt="Photo 1" class="w-full"></div>
-        <div><img src="https://source.unsplash.com/random/300x300?sig=2" alt="Photo 2" class="w-full"></div>
-        <div><img src="https://source.unsplash.com/random/300x300?sig=3" alt="Photo 3" class="w-full"></div>
-        <div><img src="https://source.unsplash.com/random/300x300?sig=4" alt="Photo 4" class="w-full"></div>
-        <div><img src="https://source.unsplash.com/random/300x300?sig=5" alt="Photo 5" class="w-full"></div>
-        <div><img src="https://source.unsplash.com/random/300x300?sig=6" alt="Photo 6" class="w-full"></div>
+    <div class="flex flex-wrap -mx-2">
+        <div class="p-2 w-1/3">
+            <img src="https://source.unsplash.com/random/300x300?sig=1" alt="Photo 1" class="w-full h-auto">
+        </div>
+        <div class="p-2 w-1/3">
+            <img src="https://source.unsplash.com/random/300x300?sig=2" alt="Photo 2" class="w-full h-auto">
+        </div>
+        <div class="p-2 w-1/3">
+            <img src="https://source.unsplash.com/random/300x300?sig=3" alt="Photo 3" class="w-full h-auto">
+        </div>
+        <div class="p-2 w-1/3">
+            <img src="https://source.unsplash.com/random/300x300?sig=4" alt="Photo 4" class="w-full h-auto">
+        </div>
+        <div class="p-2 w-1/3">
+            <img src="https://source.unsplash.com/random/300x300?sig=5" alt="Photo 5" class="w-full h-auto">
+        </div>
+        <div class="p-2 w-1/3">
+            <img src="https://source.unsplash.com/random/300x300?sig=6" alt="Photo 6" class="w-full h-auto">
+        </div>
     </div>
 </x-app-layout>
