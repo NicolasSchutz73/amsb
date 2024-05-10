9150fa2da66e2384b473a88ac166276a18bd4d1a
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-10 21:40:31 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="test" style="display: flex; justify-content: center; flex-wrap: wrap; margin: -2px;">
                 @foreach($instagram as $post)
                     <div style="width: 33.3333%; padding: 2px; box-sizing: border-box;">
-                        <a href="{{ $post->instagramUrl }}" target="_blank" style="display: block; height: 300px; background-color: #c3b1e1; overflow: hidden; position: relative;">
+                        <a href="https://www.instagram.com/amsb_test/" target="_blank" style="display: block; height: 300px; background-color: #c3b1e1; overflow: hidden; position: relative;">
                             <img src="{{ $post->url }}" alt="Instagram Post" style="width: 100%; height: 100%; object-fit: cover;">
                             <div class="overlay" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background: rgba(0, 0, 0, 0.5); opacity: 0; transition: opacity 0.3s;"></div>
                         </a>
