02168641597f50c1c53f5f8a5486940900659ab3
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-10 21:39:56 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,13 @@
         <p class="text-red-500">{{ $error }}</p>
     @else
         <div class="container mx-auto px-4 py-4" style="max-width: 1200px; margin: auto;">
-            <div class="test" style="display: flex; justify-content: flex-start; flex-wrap: wrap; margin: -2px;"> <!-- Centré et avec flex-wrap -->
+            <div class="test" style="display: flex; justify-content: center; flex-wrap: wrap; margin: -2px;">
                 @foreach($instagram as $post)
-                    <div style="width: 33.3333%; padding: 3px; box-sizing: border-box;"> <!-- Éléments de la grille -->
-                        <div style="height: 300px; background-color: #c3b1e1; overflow: hidden;"> <!-- Conteneur de l'image -->
+                    <div style="width: 33.3333%; padding: 2px; box-sizing: border-box;">
+                        <a href="{{ $post->instagramUrl }}" target="_blank" style="display: block; height: 300px; background-color: #c3b1e1; overflow: hidden; position: relative;">
                             <img src="{{ $post->url }}" alt="Instagram Post" style="width: 100%; height: 100%; object-fit: cover;">
-                        </div>
+                            <div class="overlay" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background: rgba(0, 0, 0, 0.5); opacity: 0; transition: opacity 0.3s;"></div>
+                        </a>
                     </div>
                 @endforeach
             </div>
@@ -22,15 +23,18 @@
     <style>
         .container {
             width: 100%;
-            max-width: 1200px; /* Définir une largeur maximale pour le conteneur */
-            margin: 0 auto; /* Centrer le conteneur */
+            max-width: 1200px;
+            margin: 0 auto;
             padding: 20px;
         }
         .test {
             display: flex;
-            justify-content: center; /* Centrer les éléments de la grille horizontalement */
+            justify-content: center;
             flex-wrap: wrap;
-            margin: -2px; /* Ajustement pour les marges négatives pour le spacing uniforme */
+            margin: -2px;
+        }
+        a:hover .overlay {
+            opacity: 1; /* Rendre le fond gris transparent visible au survol */
         }
     </style>
 </x-app-layout>
