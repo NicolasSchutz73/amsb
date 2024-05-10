37246d606f36cc7175b099d4982624e1d78bc6ff
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-10 21:26:30 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
         <div class="container mx-auto px-4 py-4" style="max-width: 1200px; margin: auto;">
             <div class="test" style="display: flex; justify-content: flex-start; flex-wrap: wrap; margin: -2px;"> <!-- Centré et avec flex-wrap -->
                 @foreach($instagram as $post)
-                    <div style="width: 33.3333%; padding: 12px; box-sizing: border-box;"> <!-- Éléments de la grille -->
+                    <div style="width: 33.3333%; padding: 6px; box-sizing: border-box;"> <!-- Éléments de la grille -->
                         <div style="height: 300px; background-color: #c3b1e1; overflow: hidden;"> <!-- Conteneur de l'image -->
                             <img src="{{ $post->url }}" alt="Instagram Post" style="width: 100%; height: 100%; object-fit: cover;">
                         </div>
