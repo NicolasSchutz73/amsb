4e0b4eb107cbdf16c10d45d3b772d1a51c362af9
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-11 11:22:39 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 @foreach($instagram as $post)
                     <div style="width: 33.3333%; padding: 2px; box-sizing: border-box;">
                         <a href="https://www.instagram.com/amsb_test/" target="_blank" style="display: block; height: 300px; background-color: #c3b1e1; overflow: hidden; position: relative;">
-                            <div class="overlay" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0, 0, 0, 0.5); opacity: 0;"></div>
+                            <div class="overlay" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0, 0, 0, 0.3); opacity: 0;"></div>
                             <img src="{{ $post->url }}" alt="Instagram Post" style="width: 100%; height: 100%; object-fit: cover;">
                         </a>
                     </div>
