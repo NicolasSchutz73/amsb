e2f8543e1b7fc0cb44d0253edb1b2c235c6099e9
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-13 08:44:30 +0200

    fix email

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,12 +11,11 @@
         <input type="text" id="searchBar" placeholder="Rechercher des utilisateurs..." class="mt-2 mb-4 w-64 px-4 py-2 rounded-lg border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:outline-none text-gray-900">
 
         @if (!$users->isEmpty())
-            <x-dynamic-table :headers="['Prénom', 'Nom', 'E-mail', 'Rôle', 'Action']">
+            <x-dynamic-table :headers="['Prénom', 'Nom', 'Rôle', 'Action']">
                 @foreach ($users as $user)
                     <tr class="bg-white border-b hover:bg-gray-50">
                         <td class="px-6 py-4 text-sm text-neutral-600">{{ $user->firstname }}</td>
                         <td class="px-6 py-4 text-sm text-neutral-600">{{ $user->lastname }}</td>
-                        <td class="px-6 py-4 text-sm text-neutral-600">{{ $user->email }}</td>
                         <td class="px-6 py-4">
                             @forelse ($user->getRoleNames() as $role)
                                 <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800 truncate">{{ $role }}</span>
