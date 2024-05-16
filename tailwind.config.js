2ee462de0f64860868684b297514207bcac4b2d1
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-16 17:13:58 +0200

    Vue mon équipe, couleur rouge du club,

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,20 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                red: {
+                    50: '#ffe5e5',
+                    100: '#ffcccc',
+                    200: '#ff9999',
+                    300: '#ff6666',
+                    400: '#ff3333',
+                    500: '#d81e00', // Votre couleur rouge primaire personnalisée
+                    600: '#bf1a00',
+                    700: '#a61700',
+                    800: '#8c1300',
+                    900: '#730f00',
+                },
+            },
         },
     },
 
