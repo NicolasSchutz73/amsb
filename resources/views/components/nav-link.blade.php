23ede044868485fd1b1e6128b0cfda7d4bf22856
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-17 10:28:34 +0200

    fix

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes = ($active ?? false)
                 ? 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-red-500 focus:outline-none transition duration-150 ease-in-out'
-                : 'inline-flex items-center px-1 pt-1 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-red-500 focus:outline-none focus:text-red-500 transition duration-150 ease-in-out';
+                : 'inline-flex items-center px-1 pt-1 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-red-500 hover:outline-none focus:outline-none focus:text-red-500 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
