ced1e2bf951aba3894e959f888e6973bc1782dab
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-17 10:27:31 +0200

    fix de la b border nav

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $classes = ($active ?? false)
-                ? 'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-red-500 focus:outline-none transition duration-150 ease-in-out'
-                : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-red-500 focus:outline-none focus:text-red-500 transition duration-150 ease-in-out';
+                ? 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-red-500 focus:outline-none transition duration-150 ease-in-out'
+                : 'inline-flex items-center px-1 pt-1 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-red-500 focus:outline-none focus:text-red-500 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
