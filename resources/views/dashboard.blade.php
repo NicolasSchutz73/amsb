37c1be07afc310e81ed09294abc2cd8ae8bdc5f7
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-17 10:46:57 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,8 +15,8 @@
                 @foreach($instagram as $post)
                     <div class="posts" style="padding: 2px; box-sizing: border-box;">
                         <div class="mb-8 flex flex-row items-center justify-start">
-                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                            <span class="text-base	 font-bold">Aix Maurienne Savoie Basket</span>
+                            <x-application-logo class="block h-6 w-auto fill-current"/>
+                            <span class="text-sm font-bold">Aix Maurienne Savoie Basket</span>
                         </div>
                         <a href="https://www.instagram.com/amsb_test/" target="_blank" style="display: block; height: 300px; background-color: #c3b1e1; overflow: hidden; position: relative;">
                             <div class="overlay" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0, 0, 0, 0.4); opacity: 0;"></div>
