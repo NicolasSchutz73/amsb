507274a3c464d45594fa5856bdfa11fc2993813b
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-17 10:18:10 +0200

    fix

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $classes = ($active ?? false)
-                ? 'bg-black block w-full ps-3 pe-4 py-2 border-l-4 border-red-500 text-start text-base font-medium text-red-500 focus:outline-none focus:text-red-600 focus:bg-red-100 focus:border-red-600 transition duration-150 ease-in-out'
+                ? 'bg-red-50 block w-full ps-3 pe-4 py-2 border-l-4 border-red-500 text-start text-base font-medium text-red-500 focus:outline-none focus:text-red-600 focus:bg-red-100 focus:border-red-600 transition duration-150 ease-in-out'
                 : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-red-500 hover:bg-gray-100 hover:border-gray-600 focus:outline-none focus:text-red-500 focus:bg-red-50 focus:border-red-600 transition duration-150 ease-in-out';
 @endphp
 
