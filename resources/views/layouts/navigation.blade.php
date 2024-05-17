da1ece73949d1934306fd8a2611a8e34c19c2349
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-05-17 09:53:05 +0200

    menu admin dans la nav bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,55 +3,57 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" class="">
+                    <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
-                        </x-nav-link>
-                    </div>
-
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('calendar')" :active="request()->routeIs('calendar')">
-                            {{ __('Mon agenda') }}
-                        </x-nav-link>
-                    </div>
-
-                @canany(['Admin', 'Super Admin', 'coach'])
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
-                            {{ __('Utilisateur') }}
-                        </x-nav-link>
-                    </div>
-                @endcanany
-
-                @canany(['Admin', 'Super Admin'])
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')">
-                                {{ __('Rôles') }}
-                            </x-nav-link>
-                    </div>
-                @endcanany
-
-                @canany(['Admin', 'Super Admin', 'coach'])
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('teams.index')" :active="request()->routeIs('teams.index')">
-                                {{ __('Équipes') }}
-                            </x-nav-link>
-                    </div>
-                @endcanany
-
-<!--                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('notification')" :active="request()->routeIs('notification')">
-                        {{ __('Notification') }}
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>-->
+                </div>
+
+                @canany(['Admin', 'Super Admin', 'coach'])
+                    <!-- Admin Dropdown -->
+                    <div class="hidden sm:flex sm:items-center sm:relative sm:-my-px sm:ms-10">
+                        <x-dropdown align="left" width="48">
+                            <x-slot name="trigger">
+                                <button class="px-1 pt-1 text-sm font-medium text-gray-500 bg-white rounded-md hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
+                                    {{ __('Admin') }}
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                @can('manage-users')
+                                    <x-dropdown-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                                        {{ __('Utilisateur') }}
+                                    </x-dropdown-link>
+                                @endcan
+
+                                @can('manage-roles')
+                                    <x-dropdown-link :href="route('roles.index')" :active="request()->routeIs('roles.index')">
+                                        {{ __('Rôles') }}
+                                    </x-dropdown-link>
+                                @endcan
+
+                                <x-dropdown-link :href="route('teams.index')" :active="request()->routeIs('teams.index')">
+                                    {{ __('Équipes') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                @endcanany
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('calendar')" :active="request()->routeIs('calendar')">
+                        {{ __('Mon agenda') }}
+                    </x-nav-link>
+                </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('chat-room-users')" :active="request()->routeIs('chat-room-users')">
@@ -76,7 +78,7 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->firstname }}</div>
 
                             <div class="ms-1">
